45067427b8c5b5836e177b14cf2de70e1321454e
nibab <nibab@Cezars-MacBook-Pro.local>
2016-06-04 11:54:34 -0400

    added validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :first, :last, :email, presence: true
-  validates :first, :last, length: {maximum: 50}
+  validates :first, :last, length: {maximum: 10}
   validates :email, length: {maximum: 255},
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
+                    uniqueness: { case_sensitive: false },
+                    :reduce => true
   has_secure_password
+  validates :password, length: { minimum: 6 }, presence: true, :reduce => true
+
 end
